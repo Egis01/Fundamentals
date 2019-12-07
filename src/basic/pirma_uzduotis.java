0d9a1222849d1681cabb8015fa964d59d0f15bd2
full name <egis01@gmail.com>
2019-12-07 12:14:18 +0200

    first commit

---
+++ src/basic/pirma_uzduotis.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class pirma_uzduotis {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+    }
+}
